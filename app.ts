578a0c6a92fef87161fd1c65d35cbcaad46024aa
taanyagautam <taanya.gaautam@gmail.com>
2023-05-26 22:22:55 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,7 @@
+let message: string = 'Hello, TypeScript!';
+//console.log(message);
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
